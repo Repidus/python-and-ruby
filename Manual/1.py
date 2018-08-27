96aad37d8356ff17c8f3fe90aaf09e2babdf3af3
REPIDUS <moondj123@naver.com>
2018-08-27 15:22:39 +0900

    practice chapter "Container", "Manual."

---
+++ Manual/1.py
@@ -0,0 +1,7 @@
+exlist = ['a', 'b', 'c']
+print(len(exlist))
+exlist.append('d')
+print(exlist)
+del(exlist[0])
+print(exlist)
+print(exlist[0])
---
+++ Manual/1.rb
@@ -0,0 +1,7 @@
+exlist = ['a', 'b', 'c']
+puts(exlist.length)
+exlist.push('d')
+print(exlist)
+print("\n")
+exlist.delete_at(1)
+print(exlist)
